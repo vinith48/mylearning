0f0c6029297186c605cf559be59c6fbd3f1cb05b
vinith48 <vinithkumar487@gmail.com>
2023-02-05 23:26:44 +0530

    init commit

---
+++ mylearning/src/main/java/com/vinith/mylearning/MylearningApplication.java
@@ -0,0 +1,13 @@
+package com.vinith.mylearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MylearningApplication.class, args);
+	}
+
+}
